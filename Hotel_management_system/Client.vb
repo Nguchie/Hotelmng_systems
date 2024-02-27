e5b3dd3780daad858b024bc5fe0757f8b39d6b5a
nguchie <nguchiea@gmail.com>
2024-02-27 06:43:04 +0300

    the layout for login page and client page

---
+++ Hotel_management_system/Client.vb
@@ -0,0 +1,17 @@
+﻿Public Class Client
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+End Class
